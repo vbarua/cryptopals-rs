0e9415354c3434715120fcbd0627b11438f93b9c
Victor Barua <victor.barua@gmail.com>
2021-08-26 13:20:38 -0700

    Verified repeating-key xor
    Solves Cryptopals Set 1, Challenge 5

--- src/lib.rs
+++ src/lib.rs
@@ -453,4 +453,16 @@ mod tests {
             "Now that the party is jumping\n"
         );
     }
+
+    #[test]
+    fn repeated_xor() {
+        // https://cryptopals.com/sets/1/challenges/5
+        let input = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let encrypted = xor_with_pattern(input, b"ICE");
+        let output = bytes_to_hex(&encrypted);
+        assert_eq!(
+            output,
+            "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+        );
+    }
 }
